2d966ed85cdbb5455fb3ba3b9cc30c3ebd0b7ff7
lbh2001 <1178055813@qq.com>
2021-03-23 20:56:24 +0800

    创建了拦截器

---
+++ src/main/java/com/lbh/starter/utils/Interceptor.java
@@ -0,0 +1,14 @@
+package com.lbh.starter.utils;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * @Author lbh
+ * @Date 2021/3/23
+ * @Description:拦截器
+ */
+@Component
+public class Interceptor implements HandlerInterceptor {
+
+}
